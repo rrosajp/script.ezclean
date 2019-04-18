fde7870dcbf03ae5a0877830b57939abd63db1bf
jewbmx <42426698+jewbmx@users.noreply.github.com>
2019-04-18 09:31:47 -0500

    Update service.py

--- script.ezclean/service.py
+++ script.ezclean/service.py
@@ -16,7 +16,7 @@ maxpackage_zips = int(control.getSetting('packagenumbers_alert'))
 service_thumbnails = control.getSetting('service_thumbnails')
 filesize_thumb = int(control.getSetting('filesizethumb_alert'))
 
-control.loga("[ Addon Startup ] [ v.%s ]" % control.AddonVersion)
+#control.loga("[ Addon Startup ] [ v.%s ]" % control.AddonVersion) # Removed for now, causes a error on windows10. gotta look into it lol.
 total_size = 0
 total_size2 = 0
 count = 0
@@ -34,7 +34,7 @@ for dirpath, dirnames, filenames in os.walk(packagesdir):
             choice2 = control.yesnoDialog(AddonTitle, 'The Packages folder is [B]' + str(total_sizetext) +' MB[/B] \n\n with [B]' + str(count) + '[/B] zips.', 'The folder can be cleaned up without issues to save space...', 'Do you want to clean it now?', yeslabel='Yes', nolabel='No')
             if choice2 == 1:
                 maintenance.purgePackages()
-                control.loga("[ Packages Cleared. ]")
+                #control.loga("[ Packages Cleared. ]")
 
 for dirpath2, dirnames2, filenames2 in os.walk(thumbnails):
     count2 = 0
@@ -48,20 +48,20 @@ for dirpath2, dirnames2, filenames2 in os.walk(thumbnails):
             choice2 = control.yesnoDialog(AddonTitle, 'The Thumbnails folder is [B]' + str(total_sizetext2) + ' MB[/B] \n\n with [B]' + str(count2) + '[/B] files.', 'The folder can be cleaned up without issues to save space...', 'Do you want to clean it now?', yeslabel='Yes', nolabel='No')
             if choice2 == 1:
                 maintenance.deleteThumbnails()
-                control.loga("[ Thumbnails Cleared. ]")
+                #control.loga("[ Thumbnails Cleared. ]")
 
 if auto_clean  == 'true':
     maintenance.clearCache()
-    control.loga("[ Cache Cleared. ]")
+    #control.loga("[ Cache Cleared. ]")
 
 if notify_mode == 'true':
     total_sizetext = "%.0f" % (total_size/1024000.0)
     total_sizetext2 = "%.0f" % (total_size2/1024000.0)
     control.execute('XBMC.Notification(%s, %s, %s, %s)' % (AddonTitle, 'Packages: [B]' + str(count) + '[/B] @ [B]' + str(total_sizetext) + '[/B] MB                                                                                                       Thumbnails: [B]' + str(total_sizetext2) + '[/B] MB', '6000', control.AddonIcon))
-    control.loga('Packages: ' + str(count) + ' @ ' + str(total_sizetext) + ' MB | Thumbnails: ' + str(total_sizetext2) + ' MB')
+    #control.loga('Packages: ' + str(count) + ' @ ' + str(total_sizetext) + ' MB | Thumbnails: ' + str(total_sizetext2) + ' MB')
 
 if auto_checkup  == 'true':
     from resources.lib.modules import toolz
     toolz.ForceUpdateCheck()
-    control.loga("[ Auto Check for Updates. ]")
+    #control.loga("[ Auto Check for Updates. ]")
 
